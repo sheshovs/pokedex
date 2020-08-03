f682f8a855d492ccc7c3e036083d5d846b1228b2
Sergio Vargas <sheshovs@gmail.com>
2020-08-03 02:19:54 -0400

    se agregaron flechas, se redujo el peso de imgs

--- js/main.js
+++ js/main.js
@@ -5,47 +5,28 @@ window.addEventListener('load', () => {
     var error = document.querySelector('#error');
     var infom = document.querySelector('#info');
     var random = document.querySelector('#random');
+    var prev = document.querySelector('.prev');
+    var next = document.querySelector('.next');
+    var prevResponsive = document.querySelector('#prev');
+    var nextResponsive = document.querySelector('#next');
 
-    buscar.addEventListener('click', mostrarDatos);
-    input.addEventListener('keypress', mostrarDatosEnter);
-    random.addEventListener('click',mostrarAleatorio);
+    var id_pokemon;
 
-    function mostrarDatos() {
-        var busqueda = input.value;
-        if (busqueda<1 || busqueda>809){
-            info.style.display='none';
-            input.value = '';
-            error.style.display='inline';
-            infom.style.display='none';
-        }else{
-            init(busqueda);
-            input.value = '';
-            error.style.display='none';
-            infom.style.display='none';
-        }
+
+    function errorMessage() {
+        input.value = '';
+        info.style.display = 'none';
+        error.style.display = 'inline';
+        infom.style.display = 'none';
+        prev.style.display = 'none';
+        next.style.display = 'none';
+        prevResponsive.style.display = 'none';
+        nextResponsive.style.display = 'none';
     }
-
-    function mostrarDatosEnter(e) {
-        var busqueda = input.value;
-        if (e.code == 'Enter') {
-            if (busqueda<1 || busqueda>809){
-                info.style.display='none';
-                input.value = '';
-                error.style.display='inline';
-                infom.style.display='none';
-            }else{
-                init(busqueda);
-                input.value = '';
-                error.style.display='none';
-                infom.style.display='none';
-            }
-        }
-    }
-
-    function mostrarAleatorio(){
-        infom.style.display='none';
-        random_number = Math.floor(Math.random() * 809) + 1;
-        init(random_number);
+    function infoPkm() {
+        input.value = '';
+        error.style.display = 'none';
+        infom.style.display = 'none';
     }
 
     function loadJSON(callback) {
@@ -68,90 +49,212 @@ window.addEventListener('load', () => {
     var type = document.querySelector('#type');
     var base = document.querySelector('.base');
 
-    function handleColor(tipo){
-        if(tipo == 'Grass'){
+    function handleColor(tipo) {
+        if (tipo == 'Grass') {
             return '#78C850';
-        } else if(tipo == 'Poison'){
+        } else if (tipo == 'Poison') {
             return '#A040A0';
-        } else if(tipo == 'Normal'){
+        } else if (tipo == 'Normal') {
             return '#A8A878';
-        } else if(tipo == 'Fighting'){
+        } else if (tipo == 'Fighting') {
             return '#C03028';
-        } else if(tipo == 'Flying'){
+        } else if (tipo == 'Flying') {
             return '#A890F0';
-        } else if(tipo == 'Ground'){
+        } else if (tipo == 'Ground') {
             return '#E0C068';
-        } else if(tipo == 'Rock'){
+        } else if (tipo == 'Rock') {
             return '#B8A038';
-        } else if(tipo == 'Bug'){
+        } else if (tipo == 'Bug') {
             return '#A8B820';
-        } else if(tipo == 'Ghost'){
+        } else if (tipo == 'Ghost') {
             return '#705898';
-        } else if(tipo == 'Steel'){
+        } else if (tipo == 'Steel') {
             return '#B8B8D0';
-        } else if(tipo == 'Fire'){
+        } else if (tipo == 'Fire') {
             return '#F08030';
-        } else if(tipo == 'Water'){
+        } else if (tipo == 'Water') {
             return '#6890F0';
-        } else if(tipo == 'Electric'){
+        } else if (tipo == 'Electric') {
             return '#F8D030';
-        } else if(tipo == 'Psychic'){
+        } else if (tipo == 'Psychic') {
             return '#F85888';
-        } else if(tipo == 'Ice'){
+        } else if (tipo == 'Ice') {
             return '#98D8D8';
-        } else if(tipo == 'Dragon'){
+        } else if (tipo == 'Dragon') {
             return '#7038F8';
-        } else if(tipo == 'Dark'){
+        } else if (tipo == 'Dark') {
             return '#705848';
-        } else if(tipo == 'Fairy'){
+        } else if (tipo == 'Fairy') {
             return '#EE99AC';
         }
     }
 
+    var id_actual;
+    var pokedx;
+    var busqueda;
 
     function init(id) {
-        
+
         loadJSON(function (response) {
             // Parse JSON string into object
-            var pokedex = JSON.parse(response);
-            for (var pokemon in pokedex) {
-                if (pokedex[pokemon].id == id || 
-                    pokedex[pokemon].name.english.toLowerCase() == id || 
-                    pokedex[pokemon].name.english == id || 
-                    pokedex[pokemon].name.english.toUpperCase() == id) {
-                    error.style.display='none';
-                    info.style.display = 'flex';
-                    type.innerHTML='';
-                    base.innerHTML='';
+            pokedx = JSON.parse(response);
+            id_actual = buscarPkm(pokedx, id);
 
-                    if(pokedex[pokemon].id>0 && pokedex[pokemon].id <10){
-                        imgpkm.setAttribute('src','../images/00'+pokedex[pokemon].id+'.png');
-                        var number = '00'+pokedex[pokemon].id;
-                    }else if(pokedex[pokemon].id>=10 && pokedex[pokemon].id <100){
-                        imgpkm.setAttribute('src','../images/0'+pokedex[pokemon].id+'.png');
-                        var number = '0'+pokedex[pokemon].id;
-                    }else{
-                        imgpkm.setAttribute('src','../images/'+pokedex[pokemon].id+'.png');
-                        var number = pokedex[pokemon].id;
-                    }
-                    name.innerHTML= number+ ` - `+pokedex[pokemon].name.english;
-                
-                    for (var tipo in pokedex[pokemon].type){
-                        type.innerHTML+= `<li style='background-color:${handleColor(pokedex[pokemon].type[tipo])}'>`+pokedex[pokemon].type[tipo]+`</li>`;
-                         
-                    }
-                    var bar = 1;
-                    for(var stat in pokedex[pokemon].base){
-                        base.innerHTML+= stat +': ' + pokedex[pokemon].base[stat]+
-                                `</br><progress class='stat-bar bar-${bar}' value="${pokedex[pokemon].base[stat]}" max="256"></progress></br>`;
-                        bar++;
-                    }
-                    break;
-                }else{
-                    error.style.display='inline';
-                    info.style.display='none';
-                }
+
+            if (id_actual == 1) {
+                next.addEventListener('click', () => {
+                    id_actual = buscarPkm(pokedx, id_actual + 1);
+                });
+                nextResponsive.addEventListener('click', () => {
+                    id_actual = buscarPkm(pokedx, id_actual + 1);
+                });
+            } else if (id_actual == 809) {
+                prev.addEventListener('click', () => {
+                    id_actual = buscarPkm(pokedx, id_actual - 1);
+                });
+                prevResponsive.addEventListener('click', () => {
+                    id_actual = buscarPkm(pokedx, id_actual - 1);
+                });
             }
+            else {
+                prev.addEventListener('click', () => {
+                    id_actual = buscarPkm(pokedx, id_actual - 1);
+                });
+                next.addEventListener('click', () => {
+                    id_actual = buscarPkm(pokedx, id_actual + 1);  
+                });
+                prevResponsive.addEventListener('click', () => {
+                    id_actual = buscarPkm(pokedx, id_actual - 1);
+                });
+                nextResponsive.addEventListener('click', () => {
+                    id_actual = buscarPkm(pokedx, id_actual + 1);
+                });
+            }
+
+            buscar.addEventListener('click', () => {
+                busqueda = input.value;
+
+                if(busqueda == ''){
+                    errorMessage();
+                }else if (isNaN(busqueda)) {
+                    id_actual = buscarPkm(pokedx, busqueda);
+                    if(id_actual == undefined){
+                        errorMessage();
+                    }else{
+                        infoPkm();
+                    }
+                } else if (parseInt(busqueda, 10) < 1 || parseInt(busqueda, 10) > 809) {
+                    errorMessage();
+                } else {
+                    id_actual = buscarPkm(pokedx, busqueda);
+                    infoPkm();
+                }
+            });
+
+            input.addEventListener('keypress', (e) => {
+                busqueda = input.value;
+                if (e.code == 'Enter') {
+                    if(busqueda == ''){
+                        errorMessage();
+                    }else if (isNaN(busqueda)) {
+                        id_actual = buscarPkm(pokedx, busqueda);
+                        if(id_actual == undefined){
+                            errorMessage();
+                        }else{
+                            infoPkm();
+                        }
+                    } else if (parseInt(busqueda, 10) < 1 || parseInt(busqueda, 10) > 809) {
+                        errorMessage();
+                    } else {
+                        id_actual = buscarPkm(pokedx, busqueda);
+                        infoPkm();
+                    }
+                }
+            });
+
+            random.addEventListener('click', () => {
+                infom.style.display = 'none';
+                random_number = Math.floor(Math.random() * 809) + 1;
+                id_actual = buscarPkm(pokedx, random_number);
+            });
+
         });
+
     }
+
+    function buscarPkm(pokedex, id) {
+        for (var pokemon in pokedex) {
+            if (pokedex[pokemon].id == id || pokedex[pokemon].name.english.toLowerCase() == id || pokedex[pokemon].name.english == id || pokedex[pokemon].name.english.toUpperCase() == id) {
+
+                error.style.display = 'none';
+                info.style.display = 'flex';
+                type.innerHTML = '';
+                base.innerHTML = '';
+                id_pokemon = pokedex[pokemon].id;
+
+                if (window.outerWidth >= 526) {
+                    prev.style.display = 'inline';
+                    next.style.display = 'inline';
+                    prevResponsive.style.display = 'none';
+                    nextResponsive.style.display = 'none';
+                } else {
+                    prev.style.display = 'none';
+                    next.style.display = 'none';
+                    prevResponsive.style.display = 'inline';
+                    nextResponsive.style.display = 'inline';
+                }
+
+                window.addEventListener('resize', () => {
+                    if (info.style.display == 'flex') {
+                        if (window.outerWidth >= 526) {
+                            prev.style.display = 'inline';
+                            next.style.display = 'inline';
+                            prevResponsive.style.display = 'none';
+                            nextResponsive.style.display = 'none';
+                        } else {
+                            prev.style.display = 'none';
+                            next.style.display = 'none';
+                            prevResponsive.style.display = 'inline';
+                            nextResponsive.style.display = 'inline';
+                        }
+                    }
+
+                })
+
+
+
+                var number;
+
+                if (pokedex[pokemon].id > 0 && pokedex[pokemon].id < 10) {
+                    imgpkm.setAttribute('src', '../images/00' + pokedex[pokemon].id + '.png');
+                    number = '00' + pokedex[pokemon].id;
+                } else if (pokedex[pokemon].id >= 10 && pokedex[pokemon].id < 100) {
+                    imgpkm.setAttribute('src', '../images/0' + pokedex[pokemon].id + '.png');
+                    number = '0' + pokedex[pokemon].id;
+                } else {
+                    imgpkm.setAttribute('src', '../images/' + pokedex[pokemon].id + '.png');
+                    number = pokedex[pokemon].id;
+                }
+                name.innerHTML = number + ` - ` + pokedex[pokemon].name.english;
+
+                for (var tipo in pokedex[pokemon].type) {
+                    type.innerHTML += `<li style='background-color:${handleColor(pokedex[pokemon].type[tipo])}'>` + pokedex[pokemon].type[tipo] + `</li>`;
+
+                }
+                var bar = 1;
+                for (var stat in pokedex[pokemon].base) {
+                    base.innerHTML += stat + ': ' + pokedex[pokemon].base[stat] +
+                        `</br><progress class='stat-bar bar-${bar}' value="${pokedex[pokemon].base[stat]}" max="256"></progress></br>`;
+                    bar++;
+                }
+
+                break;
+            }
+        }
+        return id_pokemon;
+    }
+
+    init();
+
+
 });
